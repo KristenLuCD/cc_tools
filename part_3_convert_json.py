e7057abf1fb6a947b9d261fcb6387bd8be85c06c
KexinLuCD <kristen_kexin_lu@hotmail.com>
2020-02-24 01:22:08 -0500

    0224_debug

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -1,6 +1,77 @@
 import cc_dat_utils
+import cc_classes
+import json
 
 #Part 3
 #Load your custom JSON file
 #Convert JSON data to CCLevelPack
+
+
+def make_level_pack_from_json(json_data):
+    new_level_pack = cc_classes.CCLevelPack()    
+
+    #Loop through the json_data
+    for level in json_data:
+        #  Create a new Level object from the json_data by reading
+        new_level = cc_classes.CCLevel()
+        #  level_number
+        new_level.level_number = level["level_number"]
+        #  time
+        new_level.time = level["time"]
+        #  num_chips
+        new_level.num_chips = level["num_chips"]
+        #  upper_layer
+        new_level.upper_layer = level["upper_layer"]
+        #  Loop through the optional_fields
+        for field in level["optional_fields"]:
+            #  Create a new Field object from the json_data by reading
+            #  field_int
+            if field["field_int"] == 3:
+                #  title
+                title = field["title"]
+                #  create a new TitleField object
+                F3 = cc_classes.CCMapTitleField(title)
+                new_level.add_field(F3)
+
+            elif field["field_int"] == 6:
+                #  password
+                password = field["password"]
+                #  create a new PasswordField object
+                F6 = cc_classes.CCEncodedPasswordField(password)
+                new_level.add_field(F6)
+
+            elif field["field_int"] == 7:
+                #  hint
+                hint = field["hint"]
+                #  create a new HintField object
+                F7 = cc_classes.CCMapHintField(hint)
+                new_level.add_field(F7)
+
+            elif field["field_int"] == 10:
+                #  Loop through the monster_move
+                coordinate = []
+                for coor in field["monster_move"]:
+                    x, y = coor[0], coor[1]
+                    # create a new Coordinate object
+                    C = cc_classes.CCCoordinate(x, y)
+                    coordinate.append(C)
+                # create a new MonsterMovementField object
+                F10 = cc_classes.CCMonsterMovementField(coordinate)
+                new_level.add_field(F10)
+      
+        new_level_pack.add_level(new_level)
+    return new_level_pack
+
+input_json_file = "data/kexinl_cc1.json"
+
+#Open the file specified by input_json_file
+#Use the json module to load the data from the file
+with open(input_json_file, "r") as reader:
+    json_data = json.load(reader)
+#Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
+make_level_pack_from_json(json_data)
+#Print out the resulting GameLibrary data using print()
+new_level_pack = make_level_pack_from_json(json_data)
+print(new_level_pack)
 #Save converted data to DAT file
+cc_dat_utils.write_cc_level_pack_to_dat(new_level_pack, "data/TEST.dat")
